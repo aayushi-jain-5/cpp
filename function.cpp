eb3d73cc4dcc2de1b9677adf6fd2be238bb227d0
aayushi-jain-5 <101443827+aayushi-jain-5@users.noreply.github.com>
2022-06-10 00:22:57 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,13 @@
+//Function is a piece of code that performs a specific task
+#include<iostream>
+using namespace std;
+int add (int n1,int n2){
+	int sum= n1+n2;
+	return sum;
+}
+int main(){
+	int a=2;
+	int b=3;
+	cout<<add(a,b)<<endl;
+	return 0;
+}
